9e30c7a5b1410c56ce32b54d7cbcf627c02ab5bd
John Stegman <greenleafservices.gls@gmail.com>
2017-03-25 14:33:15 +0000

    Changed Home Page to todo-index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "pages#home"
+  root "todos#index"
   get '/about', to: 'pages#about'
   get '/help', to: 'pages#help'
  
